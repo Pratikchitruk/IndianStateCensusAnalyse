173d953e53257cc00f9c2f62a5a348ffbe1fb40e
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-30 12:54:09 +0530

    Add files via upload

---
+++ IndianStateCensus/CSVAdapterFactory.cs
@@ -0,0 +1,22 @@
+﻿using CensusAnalyserProgram.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CensusAnalyserProgram
+{
+    class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyzer.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyzer.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+               
+                default:
+                    throw new CensusAnalyserException("No such country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
